f6144640092d47c0e9915ffda84c3cebaff1ea30
Bestama Abhi Priambada <bestama.abhi@windowslive.com>
2014-12-24 11:59:35 -0800

    tambah guilmon

--- Kelompok 6/CobaGabumon/Assets/Scripts/GuilmonController.cs
+++ Kelompok 6/CobaGabumon/Assets/Scripts/GuilmonController.cs
@@ -5,25 +5,47 @@ public class GuilmonController : MonoBehaviour {
 
 	// Use this for initialization
 	float walkTime;
+	Animator anim;
+
 	void Start () {
 		walkTime = 0f;
+		anim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (walkTime <= 4.0f) {
-			transform.Translate (Vector3.forward * 100 * Time.deltaTime);
-		} else if (walkTime > 4.0f && walkTime <= 7.48f) {
-			transform.Rotate (Vector3.down * 180 * Time.deltaTime);
-		} else if (walkTime > 7.48f && walkTime <= 16.0f) {
-			transform.Translate (Vector3.right * 100 * Time.deltaTime);
-		} else if (walkTime > 16.0f && walkTime <= 17.0f) {
-			transform.Rotate (Vector3.down * 90 * Time.deltaTime);
-		} else if (walkTime > 17.0f && walkTime <= 27.0f) {
-			transform.Translate (Vector3.forward * 100 * Time.deltaTime);
+		if (walkTime <= 4.0f) 
+		{
+			transform.Translate (Vector3.forward * 1000 * Time.deltaTime);
 		} 
+		else if (walkTime > 4.0f && walkTime <= 4.73) 
+		{
+			transform.Rotate (Vector3.down * 180 * Time.deltaTime);
+		}
+		else if (walkTime > 15.0f && walkTime <= 15.73f) 
+		{
+			transform.Rotate (Vector3.down * (-90) * Time.deltaTime);
+		} 
+		else if (walkTime > 25.75f && walkTime <= 26.52f) 
+		{
+			transform.Translate (Vector3.forward * 1000 * Time.deltaTime);
+		} 
+		else if (walkTime > 37.0f && walkTime <= 37.73f) 
+		{
+			transform.Rotate (Vector3.down * (-90) * Time.deltaTime);
+		} 
+//		else if (walkTime > 16.0f && walkTime <= 17.0f) {
+//			transform.Rotate (Vector3.down * 90 * Time.deltaTime);
+//		} else if (walkTime > 17.0f && walkTime <= 27.0f) {
+//			transform.Translate (Vector3.forward * 100 * Time.deltaTime);
+//		} 
 		
 		walkTime = walkTime + Time.deltaTime;
-		Debug.Log (walkTime);
+		var currentBaseState = anim.GetCurrentAnimatorStateInfo(0);
+		if (currentBaseState.nameHash == Animator.StringToHash("Base Layer.turn_right 0"))
+		{
+			Debug.Log(walkTime + " : walking");
+		}
+		Debug.Log (walkTime); 
 	}
 }
